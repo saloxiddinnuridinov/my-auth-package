70284e4bad0bdf8a7a87b93d64945571062967e5
saloxiddinnuridinov <nuridinovsaloxiddin@gmail.com>
2023-08-08 15:50:02 +0500

    first commit

---
+++ src/MyAuthServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace My\Auth;
+
+use Illuminate\Support\ServiceProvider;
+
+class MyAuthServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Paketni registrlash va konfiguratsiya
+    }
+
+    public function boot()
+    {
+        // Paketni boshlash va ro'yxatga olish
+    }
+}
