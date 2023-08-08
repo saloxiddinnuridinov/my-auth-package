18643d55e0d62ee690ec18dace308a8a37007d8f
saloxiddinnuridinov <nuridinovsaloxiddin@gmail.com>
2023-08-08 19:49:10 +0500

    change http

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace My\Auth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\ValidatorResponse;
--- src/routes/api.php
+++ src/routes/api.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
-Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+Route::post('register', [\My\Auth\Http\Controllers\AuthController::class, 'register']);
+Route::post('login', [\My\Auth\Http\Controllers\AuthController::class, 'login']);
